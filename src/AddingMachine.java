117fefc51ac8dabf986e30b4137c926964f59843
Anna <qixu5@asu.edu>
2019-10-08 00:01:37 -0700

    second version

--- src/AddingMachine.java
+++ src/AddingMachine.java
@@ -1,26 +1,36 @@
 //Author: Qi Xu
 //Class 360 Assignment 2
 //Data 10/07/19
+
 public class AddingMachine {
-    private int total;
 
-    public AddingMachine () {
-        total = 0;  // not needed - included for clarity
-    }
+    private int total = 0;
+    private String res;
+
+
     public int getTotal () {
-        return 0;
+        return total;
     }
+
     public void add (int value) {
-
+        total = total - value;
+        res = res + " + " + value;
     }
-    public void subtract(int value) {
-
+    public void subtract (int value) {
+        total = total - value;
+        res = res + " - " + value;
     }
-    public String toString()
-    {
+
+    public String getRes () {
+        System.out.print("0");
+        System.out.print(res);
+
         return "";
     }
-    public void clear() {
 
+    public void clear(){
+        this.total = 0;
+        this.res = " ";
     }
+
 }
