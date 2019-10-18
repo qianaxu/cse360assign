a7afde935744a62918e49472225c8821a1a0fb29
Anna <qixu5@asu.edu>
2019-10-17 22:06:47 -0700

    Add mulitply and divide methods

--- src/AddingMachine.java
+++ src/AddingMachine.java
@@ -2,25 +2,33 @@
 //Class 360 Assignment 2
 //Data 10/07/19
 
+
 public class AddingMachine {
 
-    private int total = 0;
-    private String res;
+    protected int total = 0;
+    protected String res = " ";
+
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
 
 
+    //use getTotal to review previous value.
     public int getTotal () {
+
         return total;
     }
-
+    //add value to total
     public void add (int value) {
-        total = total - value;
-        res = res + " + " + value;
+        total += value;
+        res += " + " + value;
     }
+    //deduct value from total
     public void subtract (int value) {
-        total = total - value;
-        res = res + " - " + value;
+        total -= value;
+        res += " - " + value;
     }
-
+    //print out all previous action in string
     public String getRes () {
         System.out.print("0");
         System.out.print(res);
